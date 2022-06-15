661c56065c768cd0ec84a1eb49cdabc7141ff480
zhouwenxiang <zhouwenxiang@shixiba.com>
2022-06-15 15:49:50 +0800

    eureka 初步测试完成,准备下一步使用

---
+++ winson-eureka-client-002/src/main/java/com/winson/winsoneurekaclient002/ServiceInstanceRestController.java
@@ -0,0 +1,50 @@
+package com.winson.winsoneurekaclient002;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author winson
+ * @date 2022/6/15
+ **/
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        List<ServiceInstance> serviceInstanceList = this.discoveryClient.getInstances(applicationName);
+
+        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka(applicationName, true);
+        System.out.println(instanceInfo.getHostName() + " --> "  + instanceInfo.getPort()  + " , id : " + instanceInfo.getInstanceId());
+
+        System.out.println("client-002 find service value is : " + serviceInstanceList);
+        if (serviceInstanceList != null && serviceInstanceList.size() > 0) {
+            ServiceInstance serviceInstance = serviceInstanceList.get(0);
+            String host = serviceInstance.getHost();
+            int port = serviceInstance.getPort();
+            System.out.println("service instance " + serviceInstance.getInstanceId() + " , host : " + host + " , port : " + port);
+        }
+        return serviceInstanceList;
+    }
+
+    @RequestMapping("/info2")
+    public void info2() {
+        System.out.println(" info2 method invoke ...... ");
+    }
+
+}
