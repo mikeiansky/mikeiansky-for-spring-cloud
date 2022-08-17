38f9daef135378077c28552470bdc6f89c7fe15e
zhouwenxiang <zhouwenxiang@shixiba.com>
2022-08-17 12:38:13 +0800

    security 测试

---
+++ winson-spring-secruity/src/main/java/com/winson/spring/security/WinsonSpringSecurityDemo.java
@@ -0,0 +1,17 @@
+package com.winson.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author winson
+ * @date 2022/8/15
+ **/
+@SpringBootApplication
+public class WinsonSpringSecurityDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinsonSpringSecurityDemo.class);
+    }
+
+}
